9959fe9eb6b4e8759e31d841d73b9777810d3c00
Matheus Paz <mplmatheuspaz@hotmail.com>
2024-06-07 23:07:25 -0300

    feat: Validacao para CPF, e mascara para CEP e Telefone

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
@@ -1,6 +1,8 @@
 ﻿
 $(document).ready(function () {
     $('#CPF').mask('000.000.000-00');
+    $('#CEP').mask('00000-000');
+    $('#Telefone').mask('(00) 00000-0000');
 
     if (obj) {
         $('#formCadastro #Nome').val(obj.Nome);
@@ -17,36 +19,42 @@ $(document).ready(function () {
 
     $('#formCadastro').submit(function (e) {
         e.preventDefault();
-        
-        $.ajax({
-            url: urlPost,
-            method: "POST",
-            data: {
-                "NOME": $(this).find("#Nome").val(),
-                "CEP": $(this).find("#CEP").val(),
-                "Email": $(this).find("#Email").val(),
-                "Sobrenome": $(this).find("#Sobrenome").val(),
-                "Nacionalidade": $(this).find("#Nacionalidade").val(),
-                "Estado": $(this).find("#Estado").val(),
-                "Cidade": $(this).find("#Cidade").val(),
-                "Logradouro": $(this).find("#Logradouro").val(),
-                "Telefone": $(this).find("#Telefone").val(),
-                "CPF": $(this).find("#CPF").val()
-            },
-            error:
-            function (r) {
-                if (r.status == 400)
-                    ModalDialog("Ocorreu um erro", r.responseJSON);
-                else if (r.status == 500)
-                    ModalDialog("Ocorreu um erro", "Ocorreu um erro interno no servidor.");
-            },
-            success:
-            function (r) {
-                ModalDialog("Sucesso!", r)
-                $("#formCadastro")[0].reset();                                
-                window.location.href = urlRetorno;
-            }
-        });
+
+        const cpf = $(this).find("#CPF").val();
+
+        if (!isValidCPF(cpf)) {
+            ModalDialog("Campo inválido!", "O valor do campo CPF não está no padrão correto, verifique e tente novamente.");
+        } else {
+            $.ajax({
+                url: urlPost,
+                method: "POST",
+                data: {
+                    "NOME": $(this).find("#Nome").val(),
+                    "CEP": $(this).find("#CEP").val(),
+                    "Email": $(this).find("#Email").val(),
+                    "Sobrenome": $(this).find("#Sobrenome").val(),
+                    "Nacionalidade": $(this).find("#Nacionalidade").val(),
+                    "Estado": $(this).find("#Estado").val(),
+                    "Cidade": $(this).find("#Cidade").val(),
+                    "Logradouro": $(this).find("#Logradouro").val(),
+                    "Telefone": $(this).find("#Telefone").val(),
+                    "CPF": $(this).find("#CPF").val()
+                },
+                error:
+                    function (r) {
+                        if (r.status == 400)
+                            ModalDialog("Ocorreu um erro", r.responseJSON);
+                        else if (r.status == 500)
+                            ModalDialog("Ocorreu um erro", "Ocorreu um erro interno no servidor.");
+                    },
+                success:
+                    function (r) {
+                        ModalDialog("Sucesso!", r)
+                        $("#formCadastro")[0].reset();
+                        window.location.href = urlRetorno;
+                    }
+            });
+        }
     })
     
 })
@@ -73,4 +81,49 @@ function ModalDialog(titulo, texto) {
 
     $('body').append(texto);
     $('#' + random).modal('show');
+}
+
+function isValidCPF(cpf) {
+    cpf = cpf.replace(/\D/g, '');
+
+    if (cpf.length !== 11) {
+        return false;
+    }
+
+    let allDigitsEqual = true;
+    for (let i = 1; i < cpf.length; i++) {
+        if (cpf[i] !== cpf[0]) {
+            allDigitsEqual = false;
+            break;
+        }
+    }
+
+    if (allDigitsEqual) {
+        return false;
+    }
+
+    const multiplier1 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+    const multiplier2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+    let tempCPF = cpf.substring(0, 9);
+    let sum = 0;
+
+    for (let i = 0; i < 9; i++) {
+        sum += parseInt(tempCPF[i]) * multiplier1[i];
+    }
+
+    let remainder = sum % 11;
+    let digit1 = (remainder < 2) ? 0 : (11 - remainder);
+
+    tempCPF += digit1;
+    sum = 0;
+
+    for (let i = 0; i < 10; i++) {
+        sum += parseInt(tempCPF[i]) * multiplier2[i];
+    }
+
+    remainder = sum % 11;
+    let digit2 = (remainder < 2) ? 0 : (11 - remainder);
+
+    return cpf.endsWith(digit1.toString() + digit2.toString());
 }
